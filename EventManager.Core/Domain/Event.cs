09445a8e11744c714a9d1f98022f472f001abb03
Akacja <tomasz.traczyk@onet.eu>
2018-06-25 16:43:07 +0200

    Some corrections of user model and event model.

--- EventManager.Core/Domain/Event.cs
+++ EventManager.Core/Domain/Event.cs
@@ -7,20 +7,45 @@ namespace EventManager.Core.Domain
 {
     class Event : Entity
     {
-	
+		private ISet<Signature> _modifier = new HashSet<Signature>();
+		private ISet<Ticket> _tickets;
+
 		public string Name { get; protected set; }
 		public string Description { get; protected set; }
 		public DateTime StartDate { get; protected set; }
 		public DateTime EndDate { get; protected set; }
-		public Signature Owner { get; protected set; }
-		public Signature Modifier { get; protected set; }
-		public IEnumerable<Ticket> Tickets { get; protected set; }
+		public IEnumerable<Signature> Modifier  => _modifier; 
+		public IEnumerable<Ticket> Tickets => _tickets;
 
 		protected Event() { }
-		public Event(Guid id, string name, string description, DateTime startDate, DateTime endDate, Signature owner, IEnumerable<Ticket> tickets)
+		public Event(Guid id, string name, string description, DateTime startDate, DateTime endDate, Signature creator, ISet<Ticket> tickets)
 		{
-			Tickets = tickets;
-			
+			Id = id;
+			Creator = creator;
+			UpdateEvent(name, description, startDate, endDate, null);
+			_tickets = tickets;
+		}
+		public void UpdateEvent(string name, string description, DateTime startDate, DateTime endDate, Signature modifier)
+		{
+			Name = name;
+			Description = description;
+			StartDate = startDate;
+			EndDate = endDate;
+			_modifier.Add(modifier);
+		}
+		public void AddTickets(ISet<Ticket> tickets)
+		{
+			foreach (var ticket in tickets)
+			{
+				_tickets.Add(ticket);
+			}
+		}
+		public void RemoveTickets(ISet<Ticket> tickets)
+		{
+			foreach (var ticket in tickets)
+			{
+				_tickets.Remove(ticket);
+			}
 		}
 	}
 }
--- EventManager.Core/Domain/User.cs
+++ EventManager.Core/Domain/User.cs
@@ -9,13 +9,14 @@ namespace EventManager.Core.Domain
 
 	class User : Entity
 	{
+		private ISet<Signature> _modifier = new HashSet<Signature>();
 		public string Login { get; protected set; }
 		public byte[] Password { get; protected set; }
 		public string FirstName { get; protected set; }
 		public string LastName { get; protected set; }
 		public UserRole Role { get; protected set; }
 		public UserStatus Status { get; protected set; }
-		public IEnumerable<Signature> Modifier { get; protected set; }
+		public IEnumerable<Signature> Modifier  => _modifier; 
 
 		protected User() { }
 
@@ -28,20 +29,28 @@ namespace EventManager.Core.Domain
 			Id = id;
 			Login = login;
 			Creator = creator;
-			UpdateUser( firstName, lastName, role, status, null);
+			UpdateUser( firstName, lastName, role, status, creator);
 			SetPassword(password);
 		}
-		public void UpdateUser(string firstName, string lastName, UserRole role, UserStatus status, ISet<Signature> modifier)
+		public void UpdateUser(string firstName, string lastName, UserRole role, UserStatus status, Signature modifier)
 		{
 			FirstName = firstName;
 			LastName = lastName;
 			Role = role;
 			Status = status;
-			Modifier = modifier;
+			_modifier.Add(modifier);
 		}
 		public void SetPassword(string password)
 		{
 			Password = HashHelper.CreatePassword(password);
+		}
+		public void SetRole(UserRole role)
+		{
+			Role = role;
+		}
+		public void SetStatus(UserStatus status)
+		{
+			Status = status;
 		}
 	}
 }
